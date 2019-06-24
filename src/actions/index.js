9f090cb16a5013dd03c284482d5008c439dbc85f
erozonachi <erozonachi.james@gmail.com>
2019-06-24 10:02:39 +0100

    Create and export action types

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const SUCCESS = 'SUCCESS';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const FAILURE = 'FAILURE';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const SIGNING_UP = 'SIGNING_UP';
+export const LOGGING_IN = 'LOGGING_IN';
+export const FETCHING_GUIDES = 'FETCHING_GUIDES';
+export const CREATING_GUIDE = 'CREATING_GUIDE';
+export const UPDATING_GUIDE = 'UPDATING_GUIDE';
+export const DELETING_GUIDE = 'DELETING_GUIDE';
+export const READING_GUIDE = 'READING_GUIDE';
+export const LIKING_GUIDE = 'LIKING_GUIDE';
+export const TRYING_GUIDE = 'TRYING_GUIDE';
