122e14c1d822ff9ade6188bfce051b3f77b35afe
erozonachi <erozonachi.james@gmail.com>
2019-06-24 12:18:35 +0100

    Create and export auth-form-builder component

---
+++ src/components/auths/AuthFormBuilder.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+
+export default function AuthFormBuilder(props) {
+  return(
+    <Form>
+      <Form.Field>
+        <label>Username</label>
+        <input placeholder='Enter Username' />
+      </Form.Field>
+      <Form.Field>
+        <label>Password</label>
+        <input placeholder='Enter Password' />
+      </Form.Field>
+      {
+        props.signUp &&
+          <Form.Field>
+            <label>Confirm Password</label>
+            <input placeholder='Confirm Password' />
+          </Form.Field>
+      }
+      {
+        props.signUp &&
+        <Form.Field>
+          <Checkbox label='Sign me up as a creator' />
+        </Form.Field>
+      }
+      <Button type='submit'>{(props.signUp && 'Sign Up') || 'Login'}</Button>
+    </Form>
+  );
+}
