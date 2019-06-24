7aa400c1148aeda424744f315f54eed8cb79b978
erozonachi <erozonachi.james@gmail.com>
2019-06-24 15:00:55 +0100

    Create and export form container styled component

---
+++ src/components/auths/FormContainer.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 800px;
+  width: 95%;
+  margin: 0 auto;
+  box-shadow: 4px 3px 4px 3px #ddd;
+  @media (max-width: 750) {
+    max-width: 600px;
+  }
+
+  img {
+    width: 48%;
+    @media (max-width: 750px) {
+      display: none;
+    }
+  }
+  > div {
+    width: 48%;
+    @media (max-width: 750) {
+      width:96%;
+    }
+    button {
+      width: 100%;
+    }
+  }
+`;
