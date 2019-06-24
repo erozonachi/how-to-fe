f9b7eb408c374f3f16a2d57d5092a215915149dc
erozonachi <erozonachi.james@gmail.com>
2019-06-24 11:19:31 +0100

    Wrap app component in a provider

--- src/index.js
+++ src/index.js
@@ -15,7 +15,12 @@ const store = createStore(
   applyMiddleware(thunk, logger)
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
