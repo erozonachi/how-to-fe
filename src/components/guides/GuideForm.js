e893efb566b2a8610db434e76239200988ea0641
erozonachi <erozonachi.james@gmail.com>
2019-06-25 19:43:41 +0100

    Create and export guide form component

---
+++ src/components/guides/GuideForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Button, Header, Icon, Modal, Form, Button, Divider, Transition, List, } from 'semantic-ui-react';
+
+export function GuideForm() {
+  return(
+    <Modal closeIcon>
+      <Header icon='add circle' content='Create How-to Guide' />
+      <Modal.Content>
+        <Form>
+          <Form.Field>
+            <label>Title</label>
+            <input name='title' placeholder='Enter Title' />
+          </Form.Field>
+          <Form.Field>
+            <label>Type</label>
+            <input name='type' placeholder='Enter Type' />
+          </Form.Field>
+          <Form.Field>
+            <label>Description</label>
+            <textarea name='Description' placeholder='Enter Description' />
+          </Form.Field>
+          <Form.Field>
+            <label>Steps</label>
+            <Transition.Group as={List} animation='slide left' duration={500} divided size='huge'     verticalAlign='middle'>
+              {/* {items.map(item => (
+                <List.Item key={item}>
+                  <List.Content floated='right'>
+                    <Icon name='remove' />
+                    <Icon name='edit' />
+                  </List.Content>
+                  <List.Content>Go Parry!..</List.Content>
+                </List.Item>
+              ))} */}
+            </Transition.Group>
+            <textarea name='step' placeholder='Enter a Step' />
+            <Button type='button'><Icon name='add square' /></Button>
+          </Form.Field>
+          <Form.Field>
+            <label>Image/Video Upload</label>
+            <input name='upload' type='file' />
+            <Divider horizontal>Or</Divider>
+            <label>Image/Video URL</label>
+            <input name='link' placeholder='Enter URL' />
+          </Form.Field>
+          <Button color='blue' style={{with:'100%'}} type='submit'>Submit</Button>
+        </Form>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='blue'>
+          <Icon name='remove' /> Close
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+}
---
+++ src/components/guides/index.js
@@ -0,0 +1,1 @@
+export { GuideForm } from './GuideForm';
