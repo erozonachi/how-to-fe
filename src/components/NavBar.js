1ae804eb16d2f78bc6117be65877a7d5fae007f2
erozonachi <erozonachi.james@gmail.com>
2019-06-25 10:51:25 +0100

    Place and group menu item controls

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,43 @@
 import React from 'react'
-import { Menu, Icon } from 'semantic-ui-react'
+import { Menu, Input, Icon } from 'semantic-ui-react'
 
 export function NavBar() {
  return(
-  <Menu attached='top' stackable>
-    <Menu.Item
-      name='Menu'
-      position='left'
-    >
-      <Icon name='bars' />
-    </Menu.Item>
-    <Menu.Item
-      position='left'
-    >
-      <img alt='How-to Logo' src='https://react.semantic-ui.com/logo.png' />
-    </Menu.Item>
-    <Menu.Menu
-      position='right'
-    >
-      <div className='ui right aligned category search item'>
-        <div className='ui transparent icon input'>
-          <input className='prompt' type='text' placeholder='Search...' />
-          <i className='search link icon' />
-        </div>
-        <div className='results' />
-      </div>
+  <Menu color='blue' inverted attached='top' pointing>
+    <Menu.Menu position='left'>
+      <Menu.Item
+        name='Menu'
+      >
+        <Icon name='bars' />
+      </Menu.Item>
+      <Menu.Item
+      >
+        <img alt='How-to Logo' src='https://react.semantic-ui.com/logo.png' />
+      </Menu.Item>
     </Menu.Menu>
-    <Menu.Item
-      name='AllGuides'
-      position='right'
-    >
-      <Icon name='th' />
-    </Menu.Item>
-    <Menu.Item
-      name='LikedGuides'
-      position='right'
-    >
-      <Icon name='heart outline' />
-    </Menu.Item>
-    <Menu.Item
-      name='AddGuide'
-      position='right'
-    >
-      <Icon name='add' />
-    </Menu.Item>
+
+    <Menu.Menu position='right'>
+
+      <Menu.Item>
+        <Input icon='search' placeholder='Search...' />
+      </Menu.Item>
+
+      <Menu.Item
+        name='AllGuides'
+      >
+        <Icon name='th' />
+      </Menu.Item>
+      <Menu.Item
+        name='LikedGuides'
+      >
+        <Icon name='heart outline' />
+      </Menu.Item>
+      <Menu.Item
+        name='AddGuide'
+      >
+        <Icon name='add' />
+      </Menu.Item>
+    </Menu.Menu>
 </Menu>
  );
 }
