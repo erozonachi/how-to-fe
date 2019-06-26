34169b7836231829f84325648bda4ebc16c42a12
erozonachi <erozonachi.james@gmail.com>
2019-06-26 15:24:31 +0100

    Create an enhanced axios instance with auth headers

--- src/actions/Guides.js
+++ src/actions/Guides.js
@@ -1,4 +1,11 @@
 import * as types from './index';
+import axios from 'axios';
+
+const authAxios = axios.create({
+  headers: {
+    authorization: localStorage.getItem('howToAccessToken')
+  }
+})
 
 export const openGuideForm = () => dispatch => {
   dispatch({type: types.GUIDE_FORM_OPEN});
