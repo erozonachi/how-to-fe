336b3dce3bff149a00e1f4ac00760c78e94177f6
erozonachi <erozonachi.james@gmail.com>
2019-06-27 18:21:54 +0100

    Import and use single in main_container component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import { SignUpForm, LoginForm } from './auths';
 import { Home } from './Home';
+import { Single } from './Single';
 
 export function MainContainer() {
   return(
@@ -13,6 +14,18 @@ export function MainContainer() {
           if (localStorage.getItem('howToAccessToken')) {
             return (
               <Home {...props} />
+            );
+          } else {
+            return <Redirect to='/signup' />
+          }
+        }}
+      />
+      <Route 
+        path='/guides/:id'
+        render={props => {
+          if (localStorage.getItem('howToAccessToken')) {
+            return (
+              <Single {...props} />
             );
           } else {
             return <Redirect to='/signup' />
