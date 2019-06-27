f8ea8bd23b1d5f92dca712491aa65d15188eea6c
erozonachi <erozonachi.james@gmail.com>
2019-06-27 16:49:17 +0100

    Fix render bug in single_guide_view component

--- src/components/guides/SingleGuideView.js
+++ src/components/guides/SingleGuideView.js
@@ -5,45 +5,47 @@ import { connect } from 'react-redux';
 import { mapStateToProps } from '../mapState';
 
 function SingleGuideView(props) {
-  <ViewContainer>
-    <Segment>
-      <Header as='h3'>
-        <Icon name='eye' />
-        <Header.Content>
-          {props.guidesData.singleRead.title}
-          <Header.Subheader>Type :- {` ${props.guidesData.singleRead.type}`}</Header.Subheader>
-          <Header.Subheader>Author :- {` ${props.guidesData.singleRead.username}`}</Header.Subheader>
-        </Header.Content>
-      </Header>
-      <Divider />
-      {(props.guidesData.singleRead.link.toLowerCase().match(/\.jpg$/) ||
-          props.guidesData.singleRead.link.toLowerCase().match(/\.jpeg$/) ||  
-          props.guidesData.singleRead.link.toLowerCase().match(/\.png$/))?
-          <Image alt={props.guidesData.singleRead.title} src={props.guidesData.singleRead.link} wrapped ui={false} /> :
-          (props.guidesData.singleRead.link.toLowerCase().includes('youtube')? 
-            <Embed
-              icon='play circle'
-              id={props.guidesData.singleRead.link.split('=')[1]}
-              source='youtube'
-            /> :
-            <Embed
-              icon='play circle'
-              url={props.guidesData.singleRead.link}
-            />)
-      }
-      <Segment raised>
-        {props.guidesData.singleRead.description}
+  return (
+    <ViewContainer>
+      <Segment>
+        <Header as='h3'>
+          <Icon name='eye' />
+          <Header.Content>
+            {props.guidesData.singleRead.title}
+            <Header.Subheader>Type :- {` ${props.guidesData.singleRead.type}`}</Header.Subheader>
+            <Header.Subheader>Author :- {` ${props.guidesData.singleRead.username}`}</Header.Subheader>
+          </Header.Content>
+        </Header>
+        <Divider />
+        {(props.guidesData.singleRead.link.toLowerCase().match(/\.jpg$/) ||
+            props.guidesData.singleRead.link.toLowerCase().match(/\.jpeg$/) ||  
+            props.guidesData.singleRead.link.toLowerCase().match(/\.png$/))?
+            <Image alt={props.guidesData.singleRead.title} src={props.guidesData.singleRead.link} wrapped ui={false} /> :
+            (props.guidesData.singleRead.link.toLowerCase().includes('youtube')? 
+              <Embed
+                icon='play circle'
+                id={props.guidesData.singleRead.link.split('=')[1]}
+                source='youtube'
+              /> :
+              <Embed
+                icon='play circle'
+                url={props.guidesData.singleRead.link}
+              />)
+        }
+        <Segment raised>
+          {props.guidesData.singleRead.description}
+        </Segment>
+        <Header as='h4'>Steps</Header>
+        <Segment raised>
+          <StepSection>
+            <Icon as='button' name='chevron circle left' />
+            <Container>{'Step 1'}</Container>
+            <Icon as='button' name='chevron circle right' />
+          </StepSection>
+        </Segment>
       </Segment>
-      <Header as='h4'>Steps</Header>
-      <Segment raised>
-        <StepSection>
-          <Icon as='button' name='chevron circle left' />
-          <Container>{'Step 1'}</Container>
-          <Icon as='button' name='chevron circle right' />
-        </StepSection>
-      </Segment>
-    </Segment>
-  </ViewContainer>
+    </ViewContainer>
+  );
 }
 
 export default connect(mapStateToProps, {})(SingleGuideView);
