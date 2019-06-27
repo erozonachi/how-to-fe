334ed76a013e39ff823ce8d524c99d79c2cbad87
erozonachi <erozonachi.james@gmail.com>
2019-06-27 14:20:22 +0100

    Conditionally render filtered guide list

--- src/components/guides/CardGroup.js
+++ src/components/guides/CardGroup.js
@@ -4,17 +4,20 @@ import { GroupContainer } from './StyledComponents';
 import { connect } from 'react-redux';
 import { mapStateToProps } from '../mapState';
 import { fetchGuides } from '../../actions';
+import { isArray } from 'util';
 
 function CardGroup(props) {
   const { fetchGuides } = props;
   useEffect(() => {
     fetchGuides();
   }, []);
+  const renderList = ((isArray(props.guidesData.filteredGuides) && props.guidesData.filteredGuides.length > 0) && props.guidesData.filteredGuides) || 
+    props.guidesData.guides;
 
   return(
     <GroupContainer>
       {
-        props.guidesData.guides.map(guide => <GuideCard key={guide.id} guide={guide} user={props.auth.user.username} />)
+        renderList.map(guide => <GuideCard key={guide.id} guide={guide} user={props.auth.user.username} />)
       }
     </GroupContainer>
   );
