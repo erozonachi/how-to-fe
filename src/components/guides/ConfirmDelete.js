e3beeba9692f71d5d35ce4cd5e9f12d4d26c23c3
erozonachi <erozonachi.james@gmail.com>
2019-06-27 12:50:05 +0100

    Import and use delete_guide() in confirm delete

--- src/components/guides/ConfirmDelete.js
+++ src/components/guides/ConfirmDelete.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Button, Modal } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { mapStateToProps } from '../mapState';
-import { closeConfrim, } from '../../actions';
+import { closeConfrim, deleteGuide, } from '../../actions';
 
 function ConfirmDelete(props) {
 
   const handleClose = () => props.closeConfrim();
+  const handleDelete = () => props.deleteGuide(props.guidesData.deleteID);
 
   return (
     <Modal size='mini' open={props.guidesData.confirmOpen} onClose={handleClose}>
@@ -16,10 +17,10 @@ function ConfirmDelete(props) {
       </Modal.Content>
       <Modal.Actions>
         <Button onClick={handleClose} negative>No</Button>
-        <Button positive icon='checkmark' labelPosition='right' content='Yes' />
+        <Button onClick={handleDelete} positive icon={props.guidesData.deletingGuide? 'spinner' : 'checkmark'} labelPosition='right' content='Yes' />
       </Modal.Actions>
     </Modal>
   );
 }
 
-export default connect(mapStateToProps, { closeConfrim, })(ConfirmDelete);
+export default connect(mapStateToProps, { closeConfrim, deleteGuide, })(ConfirmDelete);
